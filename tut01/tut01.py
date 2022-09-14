5702af35e5ee784346de5ea3d4a3216f925f8f94
ShantanuSSR <103843863+ShantanuSSR@users.noreply.github.com>
2022-09-14 18:51:36 +0530

    applying the function and counting

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -35,3 +35,19 @@ data_framing['U-u_mean']=data_framing['U']-data_framing.at[0,'u_mean']
 data_framing['V-v_mean']=data_framing['V']-data_framing.at[0,'v_mean']
 data_framing['W-w_mean']=data_framing['W']-data_framing.at[0,'w_mean']
 
+data_framing['octant'] = data_framing.apply(lambda y: octant_ident(y['U-u_mean'], y['V-v_mean'], y['W-w_mean']),axis=1)
+
+data_framing[' '] = ''
+data_framing.at[1,' '] = 'user input'
+
+#counting overall using value_counts function
+data_framing.at[0,'octant ID'] = 'overall count'
+data_framing.at[0,'-1'] = data_framing['octant'].value_counts()[-1]
+data_framing.at[0,'-2'] = data_framing['octant'].value_counts()[-2]
+data_framing.at[0,'-3'] = data_framing['octant'].value_counts()[-3]
+data_framing.at[0,'-4'] = data_framing['octant'].value_counts()[-4]
+data_framing.at[0,'1']  = data_framing['octant'].value_counts()[1]
+data_framing.at[0,'2']  = data_framing['octant'].value_counts()[2]
+data_framing.at[0,'3'] = data_framing['octant'].value_counts()[3]
+data_framing.at[0,'4'] = data_framing['octant'].value_counts()[4]
+
