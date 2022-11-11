33efc4f10a5a227d72d9762e61e00e93aad62ebc
ShantanuSSR <103843863+ShantanuSSR@users.noreply.github.com>
2022-11-11 22:40:44 +0530

    Code completed
    Writing final code.

--- tut05/tut05.py
+++ tut05/tut05.py
@@ -258,3 +258,29 @@ def octant_identification(mod):
         
     # calling count_in_range function to find the octant sign
     count_in_range(mod)
+
+from platform import python_version
+ver = python_version()
+
+if ver == "3.8.10":
+    print("Correct Version Installed")
+else:
+    print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
+
+mod=5000
+
+try:
+    octant_identification(mod)
+except NameError:
+    print('function does not exist')
+try:
+    octant_ranking(mod)
+except NameError:
+    print('function does not exist')
+
+#saving the output file
+wb.save('octant_output_ranking_excel1.xlsx')
+
+#This shall be the last lines of the code.
+end_time = datetime.now()
+print('Duration of Program Execution: {}'.format(end_time - start_time))
