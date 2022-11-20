9c3693506a6481a351040fd6087024a7da55ba2c
ShantanuSSR <103843863+ShantanuSSR@users.noreply.github.com>
2022-11-20 21:47:37 +0530

    splitting given input into days, months and year

--- tut06/tut06.py
+++ tut06/tut06.py
@@ -24,3 +24,25 @@ def attendance_report():
         rollno_inp=pd.read_csv('input_registered_students.csv')          ##reading the registered student file
     except:
         print('File containing name of all students is missing!')
+    mc=sum(1 for row in open("input_registered_students.csv"))         ##getting the size of dataframe
+    mc_consolidated=sum(1 for row in open("input_attendance.csv"))
+    total_dates=list()
+    for j in range(0,mc_consolidated-1):
+        day=inp.at[j,'Timestamp'].split()[0].split('-')[0]
+        month=inp.at[j,'Timestamp'].split()[0].split('-')[1]
+        year=inp.at[j,'Timestamp'].split()[0].split('-')[2]
+        date=datetime.strptime(f'{year}-{month}-{day}', "%Y-%m-%d").date()
+        day_name=date.strftime("%A")
+        if day_name=="Monday" or day_name=="Thursday":
+            if inp.at[j,'Timestamp'].split()[0] not in total_dates:
+                total_dates.append(inp.at[j,'Timestamp'].split()[0]) ##getting the total dates on which lectures were held
+    
+                                                                    
+    fileName_consolidated=".\output\\attendance_report_consolidated.xlsx"
+    try:
+        output_file=openpyxl.Workbook()
+        output=output_file.active
+        output_file.save(fileName_consolidated)
+    
+    except:
+        print("Code is not working")
